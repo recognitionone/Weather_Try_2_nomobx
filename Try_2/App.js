9cb66f5b0a15027fb84e19c105018df992213148
recognitionone <recognitionone@gmail.com>
2019-01-28 20:38:30 +0100

    first commit

---
+++ Try_2/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
+
+import { API_KEY } from './utils/WeatherAPIKey';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+};
+
+componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Gettig Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          isLoading: false
+          });
+      });
+}
+
+  render() {
+    const { isLoading, weatherCondition, temperature } = this.state;
+    return (
+      <View style={styles.container}>
+        { isLoading ? (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}>Fetching The Weather</Text>
+          </View>
+        ) : (
+          <View style={styles.loadingContainer}>
+            <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}> The Weather</Text>
+          </View>
+
+          <Weather weather={weatherCondition} temperature={temperature} />
+          </View>
+        ) }
+      </View>
+    );
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFDE4'
+  },
+  loadingText: {
+    fontSize: 30
+  }
+});
